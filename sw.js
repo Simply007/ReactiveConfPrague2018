97c5cc6c2b39fb4a0723669180829e744ef9bd37
Simply007 <chrastina.ondra@gmail.com>
2018-10-30 14:59:06 +0100

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c73a9ff352bf097b735f.js"
+    "url": "webpack-runtime-407e35519b9fd9805967.js"
   },
   {
     "url": "app-c51ed8506ffd4e237caf.js"
@@ -36,11 +36,11 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "a4b6f8baf0656eb0dc3f53925c4b8865"
+    "revision": "de06c52b381ac5206b7b7d904caeed75"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "26224348a6ec1d07617ce5c6a4363fd3"
+    "revision": "be28ac621cf3c6293f2ca7aada56b9b9"
   },
   {
     "url": "0.1d82d0ccad5001634c3e.css"
@@ -52,8 +52,8 @@ self.__precacheManifest = [
     "url": "component---src-pages-index-js-6e44d07d4d0b29e534b3.js"
   },
   {
-    "url": "static/d/348/path---index-6a9-DfdWpHScbNAe7O5FQ3C7GKu22M.json",
-    "revision": "e30240be19b0e15e151910230a95f430"
+    "url": "static/d/532/path---index-6a9-Lulv12gdTz7ewhmFqi14rSg3H34.json",
+    "revision": "02e96a0904bb207f7e3127ad2a70fca1"
   },
   {
     "url": "component---src-pages-404-js-abe621e2f52f7cdf9f65.js"
